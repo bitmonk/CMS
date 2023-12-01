4716d8ee0727f6dee4f6d4f9dbcf417ee42c9ff0
RyanLama <parbattamang1234@gmail.com>
2023-12-01 21:48:06 +0545

    configure database for request and response

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const app = require("express")();
+const express = require("express")
+const app = express();
 
 const { connectDatabase } = require("./database/dabasase");
+const Blog = require("./model/blogModel");
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 connectDatabase()
+
 //GET API
 app.get("/", (req, res) => {
     res.json({
         status : 200,
         message : "Success"
+    })
+})
+
+
+//GET API => /blogs (All blogs) 
+app.get("/blogs", async (req, res) => {
+
+    const blogs = await Blog.find()
+
+    res.json({
+        status : 200,
+        message : "All Blogs",
+        data : blogs
+    })
+})
+
+
+//CREATE BLOG API
+app.post("/createBlog", async (req, res) => {
+    console.log(req.body)
+
+    await Blog.create({
+        title : req.body.title,
+        subTitle : req.body.subTitle,
+        description : req.body.description
+    })
+
+
+
+    res.json({
+        status : 200,
+        message : "Blog created successfully"
     })
 })
 
